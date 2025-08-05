1b290042cfbef0d736296e423364b261716ef4c8
macmini2019 <info@hsmtrych.net>
2025-08-05 12:21:04 +0900

    stylelint更新

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-  "singleQuote": true,
-  // "doubleQuote": true,
-  "trailingComma": "all",
-  "arrowParens": "always",
-  "tabWidth": 2,
+  "singleQuote": false,        // Stylelintのダブルクォートルールに合わせる
   "trailingComma": "es5",
+  "arrowParens": "always",
+  "tabWidth": 2,               // Stylelintのindentation: 2に対応
+  // CSS/SCSS専用設定を追加
+  "overrides": [
+    {
+      "files": ["*.scss", "*.css"],
+      "options": {
+        "singleQuote": false,    // CSSではダブルクォート
+        "tabWidth": 2
+      }
+    }
+  ]
 }
